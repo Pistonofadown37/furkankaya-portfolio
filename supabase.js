a41e2636aa099ea0c42ac6311aaeadb79e6c4945
Pistonofadown37 <frkngrapichs@gmail.com>
2026-08-27 22:46:11 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,13 +2,15 @@
 // SUPABASE BAĞLANTISI
 // =============================================
 
-const SUPABASE_URL = "https://fwlanmbmintingmruzty.supabase.co";
+const SUPABASE_URL =
+    "https://fwlanmbmintingmruzty.supabase.co";
 
 const SUPABASE_ANON_KEY =
     "sb_publishable__hwI8XK5QfD0VIMkc-j-Lw_k-_XG8Ie";
 
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
