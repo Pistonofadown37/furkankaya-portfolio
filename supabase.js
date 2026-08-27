1831e7b851feeca632c611ab25e665608a2f05a0
Pistonofadown37 <frkngrapichs@gmail.com>
2026-08-27 23:16:38 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,27 +1,32 @@
 // =============================================
-// FURKAN KAYA PORTFOLIO
 // SUPABASE BAĞLANTISI
 // =============================================
 
-
-// =============================================
-// SUPABASE PROJE BİLGİLERİ
-// =============================================
-
-const SUPABASE_URL =
-    "https://fwlanmbmintingmruzty.supabase.co";
-
-
-const SUPABASE_ANON_KEY =
-    "sb_publishable__hwI8XK5QfD0VIMkc-j-Lw_k-_XG8Ie";
-
-
-// =============================================
-// SUPABASE CLIENT
-// =============================================
-
-const supabaseClient =
-    supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_ANON_KEY
+// Supabase CDN yüklü mü kontrol et
+if (typeof supabase === "undefined") {
+    console.error(
+        "Supabase kütüphanesi yüklenemedi."
     );
+} else {
+
+    const SUPABASE_URL =
+        "https://fwlanmbmintingmruzty.supabase.co";
+
+    const SUPABASE_ANON_KEY =
+        "sb_publishable__hwI8XK5QfD0VIMkc-j-Lw_k-_XG8Ie";
+
+
+    // Global olarak oluştur
+    window.supabaseClient =
+        supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_ANON_KEY
+        );
+
+
+    console.log(
+        "Supabase bağlantısı hazır:",
+        window.supabaseClient
+    );
+
+}
